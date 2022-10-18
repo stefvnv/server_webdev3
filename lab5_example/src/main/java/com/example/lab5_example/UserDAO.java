e7a2d24bb2c91f421fa511b5bb666028661ee042
stefvnv laptop <a00282343@student.ait.ie>
2022-10-18 12:28:46 +0100

    lab5 started

---
+++ lab5_example/src/main/java/com/example/lab5_example/UserDAO.java
@@ -0,0 +1,27 @@
+package com.example.lab5_example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum UserDAO {
+    instance;
+
+    List<User> users;
+
+    private UserDAO(){
+        users = new ArrayList<User>(3);
+
+        User user1 = new User("Paul", "Athlone");
+        users.add(user1);
+
+        User user2 = new User("Johnny", "New York");
+        users.add(user2);
+
+        User user3 = new User("Clementine", "Dublin");
+        users.add(user3);
+    }
+
+    public List <User> list(){
+        return users;
+    }
+}
